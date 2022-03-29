a5a20039fda9da64ece2846493da160bfe3bbe36
Andre Blaze <kuraiokamiblaze@gmail.com>
2022-03-28 19:06:15 -0600

    Task 2 done thru episode Component

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -1,10 +1,47 @@
 import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
+import { render, fireEvent, screen, getByAltText } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Episode from './../Episode';
 
-test("renders without error", () => { });
+const testEpisodeData = {
+    id:1000, 
+    image: "https://www.animationmagazine.net/wordpress/wp-content/uploads/One-Piece-1000-Commemorative-Battle-of-Onigashima-Visual-1000x600.jpg", 
+    name: "Overwhelming Strength! The Straw Hats Come Together", 
+    season: 20, 
+    number: 27, 
+    summary: "This is the anime's 1,000th episode and some features were changed to signify it", 
+    runtime: 24
+}
+const testDataNullImg = { 
+    id:1000, 
+    image: null, 
+    name: "Overwhelming Strength! The Straw Hats Come Together", 
+    season: 20, 
+    number: 27, 
+    summary: "This is the anime's 1,000th episode and some features were changed to signify it", 
+    runtime: 24
+}
 
-test("renders the summary test passed as prop", () => { });
+test("renders without error", async () => { 
+    render(<Episode episode={testEpisodeData}/>)
+    await screen.findByText('Overwhelming Strength!', { exact: false })
 
-test("renders default image when image is not defined", () => { });
+}); // Complete
+
+test("renders the summary test passed as prop", () => { 
+    render(<Episode episode={testEpisodeData}/>)
+
+    const summary = screen.getByText(/anime/i, { exact: false })
+    const summaryNotThere = screen.queryByText('summary', { exact: false })
+
+    expect(summary).toBeVisible()
+    expect(summary).toBeInTheDocument()
+    expect(summaryNotThere).toBe(null)
+}); // Complete
+
+test("renders default image when image is not defined", async () => { 
+    render(<Episode episode={testDataNullImg}/>)
+    const defaultImg = screen.getByAltText(/stranger-things.png/, { exact: false })
+    expect(defaultImg).toBeVisible()
+});
+
